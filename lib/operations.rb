6265181e0bafe8b18b75a4551550693d742ce5ab
active-object-6703 <nerajno@gmail.com>
2019-01-16 14:26:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,12 @@
 require 'pry'
 
 def unsafe?(speed)
-	if (speed <= 60) && (speed >=40) 
+	if (speed <= 60) && (speed >=40)
 		return true
 	else
 		return false
 end
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
